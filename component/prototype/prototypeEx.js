d60fb81f36c24b97828f0122be0e371433cb6ed3
Vaibhavi7499 <vaibhavibodke13@gmail.com>
2025-04-28 14:02:41 +0530

    optional chaining in javascript

--- component/prototype/prototypeEx.js
+++ component/prototype/prototypeEx.js
@@ -17,3 +17,18 @@ Person.prototype.getDetails = function () {
 let p = new Person("xyz", 19);
 console.log(p); // {name:"xyz" , age:19}
 p.getDetails();
+
+/* optional chaining allows you to access deeply nested properties of an object
+  without having to check if each property in the chain is null or undefined.
+  If object is null or undefined it doesnt throw an error instead if returns 
+  undefined */
+
+let Person = {
+  name: "xyz",
+  addrss: {
+    city: "pune",
+  },
+};
+
+console.log(Person.address.location); // throw arror
+console.log(Person?.address?.location); // undefined

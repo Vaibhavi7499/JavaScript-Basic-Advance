be65be5677ebc8f0ae8c8a4d85b1f656a8db0158
Vaibhavi7499 <vaibhavibodke13@gmail.com>
2025-04-28 13:53:04 +0530

    Add methods to a function using prototype

--- component/prototype/prototypeEx.js
+++ component/prototype/prototypeEx.js
@@ -3,3 +3,17 @@ Array.prototype.accessName = function (ele) {
 };
 
 arr.accessName("xyz");
+
+//function as a constructor
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+Person.prototype.getDetails = function () {
+  console.log(`My name is ${this.name} and age is ${this.age}`);
+};
+
+let p = new Person("xyz", 19);
+console.log(p); // {name:"xyz" , age:19}
+p.getDetails();

74eed9e2370c2192dfc4132bb1c7a4208c7e189a
Vaibhavi7499 <vaibhavibodke13@gmail.com>
2025-04-30 12:13:00 +0530

    Create object using constructor

--- component/objectCreateWays/objectCreation.js
+++ component/objectCreateWays/objectCreation.js
@@ -10,3 +10,19 @@ let Person = {
 };
 
 console.log(Person.accessDetails());
+
+//2. Object with a constructor
+
+class Car {
+  constructor(color, model) {
+    this.color = color;
+    this.model = model;
+  }
+
+  drive = function () {
+    console.log(`car color is ${this.color} & model is ${this.model}`);
+  };
+}
+
+let c = new Car("white", "tata-punch");
+console.log(c.drive());

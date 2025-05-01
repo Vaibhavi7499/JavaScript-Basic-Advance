a6a981394fbc7f52eebcfa3b788208701b4592f8
Vaibhavi7499 <vaibhavibodke13@gmail.com>
2025-05-01 12:25:57 +0530

    Simple example in js

--- component/memorization/memorization.js
+++ component/memorization/memorization.js
@@ -16,3 +16,20 @@ function memorization() {
 
 let a = memorization();
 a(5);
+
+let fruits = ["apple", "banana", "mango", "banana", "apple", "apple"];
+let obj = {};
+fruits.forEach((ele) => {
+  if (obj[ele]) {
+    obj[ele] = obj[ele] + 1;
+  } else {
+    obj[ele] = 1;
+  }
+});
+console.log(obj);
+//it 1 {apple : 1}
+// it 2 {apple : 1, mango : 1}
+// it 3 {apple : 1, mango : 1, banana : 1}
+// it 4 {apple : 2, mango : 1, banana : 1}
+// it 5 {apple : 3, mango : 1, banana : 1}
+// it 5 {apple : 3, mango : 2, banana : 1}
